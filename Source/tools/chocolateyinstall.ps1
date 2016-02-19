a52d9cbf2538017745b47e0b810e835d6f67a592
Kevin Nord <knord@meredith.com>
2016-02-19 09:07:26 -0800

    Added install/uninstall files

---
+++ Source/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+
+$packageName= 'GitKraken' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://release.gitkraken.com/win32/GitKrakenSetup.exe' # download url
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' #only one of these: exe, msi, msu
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs   = '/S'           # NSIS
+  validExitCodes= @(0) #please insert other valid exit codes here
+
+  # optional, highly recommended
+  softwareName  = 'GitKraken*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = ''
+  checksumType  = 'md5' #default is md5, can also be sha1
+  checksum64    = ''
+  checksumType64= 'md5' #default is checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
